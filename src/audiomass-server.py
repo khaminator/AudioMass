4d020edef90fa2abe91a68ece34cdc112a609842
Kinzie Hamilton <70981891+khaminator@users.noreply.github.com>
2022-12-13 11:27:36 -0700

    Update audiomass-server.py

--- src/audiomass-server.py
+++ src/audiomass-server.py
@@ -3,5 +3,5 @@ import SocketServer
 PORT = 5055 
 Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
 httpd = SocketServer.TCPServer(("", PORT), Handler)
-print "serving at port", PORT
+print("serving at port", PORT)
 httpd.serve_forever()
